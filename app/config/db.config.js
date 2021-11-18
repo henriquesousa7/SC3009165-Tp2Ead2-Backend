2525822b96dbfb2dce206131373a7882af08e9c4
henriquesousa7 <henrique.devsousa@gmail.com>
2021-11-18 09:23:26 -0300

    Refat config db

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    url: "mongodb.srv://sc3009165:12345@cluster0.uapdq.mongodb/bancoTeste?retryWrites=true&w=majority"
+    url: "mongodb+srv://sc3009165:12345@cluster0.uapdq.mongodb/bancoTeste?retryWrites=true&w=majority"
 };
